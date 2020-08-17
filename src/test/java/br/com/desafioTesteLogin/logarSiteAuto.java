3f0d8391c687d900e44706e92ed1f0a4eeebafe7
unknown <udson.willams@gmail.com>
2020-08-17 17:21:30 -0300

    desafio de logar na page guru99

---
+++ src/test/java/br/com/desafioTesteLogin/logarSiteAuto.java
@@ -0,0 +1,28 @@
+package br.com.desafioTesteLogin;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class logarSiteAuto {
+
+    screenShot print = new screenShot();
+
+    @Test
+    public void loginAuto() throws Exception {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.navigate().to("http://demo.guru99.com/test/login.html");
+        driver.findElement(By.name("email")).sendKeys("udsonwillams@gmail.com");
+        driver.findElement(By.name("passwd")).sendKeys("123987");
+        print.takeSnapShot(driver, "d:\\testeLogin.png") ;
+        driver.findElement(By.id("SubmitLogin")).click();
+        Assert.assertEquals("Successfully Logged in...", driver.findElement(By.xpath("/html/body/div[2]/div/div/h3")).getText());
+        print.takeSnapShot(driver, "d:\\testeLoginConfirmacao.png") ;
+        driver.quit();
+    }
+}   
---
+++ src/test/java/br/com/desafioTesteLogin/screenShot.java
@@ -0,0 +1,21 @@
+package br.com.desafioTesteLogin;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+
+public class screenShot {
+    public static void takeSnapShot(WebDriver webdriver, String fileWithPath) throws Exception {
+//Converte o objeto web driver para tirar um print
+        TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+//chamao metodo getScreenshotAs para criar uma imagem
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+//Move o arquivo de imagem para o novo destino
+        File DestFile = new File(fileWithPath);
+//Copia o arquivo no novo destino
+        FileUtils.copyFile(SrcFile, DestFile);
+    }
+}
